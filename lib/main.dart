c62dbdd72123c602589d4536d08426e503c7035b
Abdun Nayeem Khan <nayeem0073297@gmail.com>
2021-10-28 07:11:17 +0600

    first comm

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:black_list/page/home_page.dart';
+import 'package:black_list/provider/todos.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Black List';
+
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => TodosProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            scaffoldBackgroundColor: Color(0xFFf6f5ee),
+          ),
+          home: HomePage(),
+        ),
+      );
+}
